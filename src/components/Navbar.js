31c609ce06ec593f1cdc6c4c09a0c55b96849d6c
Index4838 <92106337+Index4838@users.noreply.github.com>
2024-08-13 14:01:00 +0500

    New project is initialized

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import {Nav,Navbar,Container} from 'react-bootstrap';
+export const NavBar = () => {
+    return(
+        <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    );
+}
